0eef9cc0de91acc30fc48fe4f5c239b46220d0b4
Adam Rich <adam.rich@beazley.com>
2020-12-16 06:49:45 -0500

    2020-12-16- 6-49-44

--- common.R
+++ common.R
@@ -38,3 +38,65 @@ require <- function(...) {
 
 
 
+
+info <- function(x, ...) {
+  UseMethod("info")
+}
+
+# [ALR Nov 19, 2020]
+# Edited to show data type and some info on non-numeric columns
+info.data.frame <- function(dataframe) {
+  
+  out <- NULL
+  for (i in 1:ncol(dataframe)) {
+    
+    v <- dataframe[, i, drop = TRUE]
+    num <- is.numeric(v) | is.logical(v)
+    lv <- length(v)
+    vn <- v[!is.na(v)]
+    uniques <- length(unique(vn))
+    typ <- class(v)
+    
+    if (num) {
+      qv <- stats::quantile(
+        vn, 
+        probs = c(0, 0.25, 0.5, 0.75, 1))
+      sdv <- sd(vn)
+      muv <- mean(vn)
+      zero_count = sum(vn == 0)
+      lvls <- NA
+    } else {
+      qv <- rep(NA, 5)
+      sdv <- NA
+      muv <- NA
+      zero_count = NA
+      lvls <- table(v) %>% sort(decreasing = TRUE) %>% 
+        names %>% `[`(., 1:min(10, length(.))) %>% paste0(collapse = ', ')
+    }
+    
+    qvr <- data.frame(
+      row.names = i,
+      column = names(dataframe)[i],
+      type = typ,
+      min = qv[1],
+      q25 = qv[2],
+      median = qv[3],
+      mean = muv,
+      q75 = qv[4],
+      max = qv[5],
+      sd = sdv,
+      zero_count,
+      # sd2ratio = sum(abs(vn - muv) < 2 * sdv) / length(vn),
+      na_count = lv - length(vn),
+      unique_values = uniques,
+      levels = lvls)
+    
+    if (is.null(out)) { 
+      out <- qvr } else { out <- rbind(out, qvr) }  
+    
+  }
+  return(out)
+}
+
+
+
